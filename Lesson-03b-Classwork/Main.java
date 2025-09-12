a083d693825b91faf8c23d0f9b0f2822abc72c5c
A-Id1ot <gordont13@nycstudents.net>
2025-09-12 13:55:00 +0000

    asd

---
+++ Lesson-03b-Classwork/Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+   System.out.println("Gordon Tseng \nSpace Needle, 400 Broad St");
+    
+  }
+
+  
+}
