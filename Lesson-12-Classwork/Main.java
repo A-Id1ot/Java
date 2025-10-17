ffa77bccc0926df2dcf6a0d44cf3f50be6093e19
A-Id1ot <gordont13@nycstudents.net>
2025-10-17 14:16:09 +0000

    asd

---
+++ Lesson-12-Classwork/Main.java
@@ -0,0 +1,27 @@
+class Main {
+
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init(){
+   
+
+    
+  }
+  boolean collegeCredit(String co){
+    if(co.substring(5,6).equals("M")||co.substring(5,6).equals("E")){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  String getCounsoler(String fi, String mi){
+    if co.substring(0,1).equals("C"){
+
+    }
+  }
+}
