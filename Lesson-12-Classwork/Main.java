f3138365e0481b8e41d4b68c3bcb189cfc009abc
A-Id1ot <gordont13@nycstudents.net>
2025-10-20 23:26:36 +0000

    asd

--- Lesson-12-Classwork/Main.java
+++ Lesson-12-Classwork/Main.java
@@ -7,9 +7,7 @@ class Main {
   void printt(Object o){ System.out.print(o);}
 
   void init(){
-   
-
-    
+    System.out.println(collegeCredit("asdfasdf"));
   }
   boolean collegeCredit(String co){
     if(co.substring(5,6).equals("M")||co.substring(5,6).equals("E")){
@@ -20,8 +18,41 @@ class Main {
     }
   }
   String getCounsoler(String fi, String mi){
-    if co.substring(0,1).equals("C"){
-
+    if(fi.substring(0,1).equals("B")||mi.substring(0,1).equals("B")){
+      return "Berrouet";
+    }
+    else if(fi.substring(0,1).equals("C")||mi.substring(0,1).equals("C")){
+      return "Chu";
+    }
+    else if(fi.substring(0,1).equals("D")||mi.substring(0,1).equals("D")){
+      return "Dinn";
+    }
+    else if(fi.substring(0,1).equals("E")||mi.substring(0,1).equals("E")){
+      return "Eyzengart";
+    }
+    else if(fi.substring(0,1).equals("F")||mi.substring(0,1).equals("F")){
+      return "Flores";
+    }
+    else if(fi.substring(0,1).equals("G")||mi.substring(0,1).equals("G")){
+      return "Gibson";
+    }
+    else if(fi.substring(0,1).equals("L")||mi.substring(0,1).equals("L")){
+      return "Shim Lee";
+    }
+    else if(fi.substring(0,1).equals("M")||mi.substring(0,1).equals("M")){
+      return "Meltzer";
+    }
+    else if(fi.substring(0,1).equals("P")||mi.substring(0,1).equals("P")){
+      return "Paccione";
+    }
+    else if(fi.substring(0,1).equals("S")||mi.substring(0,1).equals("S")){
+      return "Fiore";
+    }
+    else if(fi.substring(0,1).equals("T")||mi.substring(0,1).equals("T")){
+      return "Tsai";
+    }
+    else{
+      return "Zayes";
     }
   }
 }
