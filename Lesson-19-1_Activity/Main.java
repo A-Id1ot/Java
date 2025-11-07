f4a6def3781a3d079a111596254b16fd4ac0cf60
A-Id1ot <gordont13@nycstudents.net>
2025-11-07 15:02:55 +0000

    asd

---
+++ Lesson-19-1_Activity/Main.java
@@ -0,0 +1,39 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  void print(Object o){System.out.println(o);}
+  void printt(Object o){System.out.print(o);}
+  void init(){
+    print(rollDice());
+    print(lotto());
+    print(additionTutor());
+  }
+  String rollDice(){
+    int fr=(int)(Math.random()*6)+1;
+    int sr=(int)(Math.random()*6)+1;
+    String a=fr+" "+sr;
+    return a;
+  }
+  String lotto(){
+    String bld="";
+    for(int x=0;x<5;x++){
+      int rr=(int)(Math.random()*48)+1;
+      bld+=rr+" ";
+    }
+    return bld;
+  }
+  String additionTutor(){
+    int fn=(int)(Math.random()*600)+1;
+    int sn=(int)(Math.random()*600)+1;
+    print("What is the answer:");
+    print(fn + " + " + sn + " = ? ");
+    int a=Input.readInt();
+    if(a==fn+sn){
+      return "Correct!";
+    }
+    else{
+      return "Incorrect.";
+    }
+  }
+}
