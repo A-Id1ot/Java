09eb70dc42dfe4e686007b428164c150bcd62029
A-Id1ot <gordont13@nycstudents.net>
2025-12-08 14:45:24 +0000

    asd

--- Lesson-25-Classwork/Lesson-25-Classwork/Main.java
+++ Lesson-25-Classwork/Lesson-25-Classwork/Main.java
@@ -5,37 +5,6 @@ class Main {
   void print(Object o){ System.out.println(o);}
   void printt(Object o){ System.out.print(o);}
   void init(){
-    // Challenge1: Read in the file Original.txt and store it in a variable
-    String msg=Input.readFile("Original.txt");
-    print(encode(msg));
-    print(decode(encode(msg)));
-  }
-  // Challenge2: 
-  //  Write the function encode that takes in a string
-  //  and create a new string that using a caesar cipher 
-  // by shifting all ascii values by 3.  Don't worry about
-  // wrapping arount the alphabet.
-  String encode(String m){
-    String bld="";
-    for(int i=0;i<m.length();i++){
-      char c=m.charAt(i);
-      c=(char)(c+3);
-      bld+=c;
-    }
-    return bld;
-  }
-  // Challenge3: 
-  //  Write the function decode that takes in a string
-  //  and reverses what the function in chanllenge 2
-  //  so that you get the original message.
-  String decode(String m){
-    String bld2="";
-    for(int i=0;i<m.length();i++){
-      char c = m.charAt(i);
-      c=(char)(c-3);
-      bld2+=c;
-    }
-    return bld2;
   }
   int randInt(int lower, int upper){
     int range = upper - lower;
