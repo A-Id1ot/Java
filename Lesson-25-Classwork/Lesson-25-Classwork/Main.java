aa52224586ff2dc4888fd8c325e5098198d7a103
A-Id1ot <gordont13@nycstudents.net>
2025-12-09 14:45:13 +0000

    asd

--- Lesson-25-Classwork/Lesson-25-Classwork/Main.java
+++ Lesson-25-Classwork/Lesson-25-Classwork/Main.java
@@ -5,28 +5,30 @@ class Main {
   void print(Object o){ System.out.println(o);}
   void printt(Object o){ System.out.print(o);}
   void init(){
-    String myStr = "Hi yo";
+    String m = "Hi yo";
+    System.out.println(e(m));
   }
+  int e(String s){
     int randomNum = (int)(Math.random() * 255);
     System.out.println(randomNum);
     String newstring = "";
-    int[] newarray = new int[myStr.length()];
-    System.out.println(myStr);
-    for(int i = 0; i < myStr.length(); i++){
-    	int result = myStr.charAt(i);
+    int[] newarray = new int[s.length()];
+    System.out.println(s);
+    for(int i = 0; i < s.length(); i++){
+    	int result = s.charAt(i);
         newarray[i] = result+randomNum;
-		System.out.println(newarray[i]);
-    }
+      }
     int[] suffering = new int[newarray.length+1];
     for(int i = 0; i < newarray.length; i++){
     	if(newarray[i] % 4 == 0){
         	suffering[i] = newarray[i]/4;
-        }
-        else{
+      }
+      else{
      		suffering[i] = newarray[i]*4;
-        }
-		System.out.println(suffering[i]);
+      }
     }
+    return randomNum;
+  }
   int randInt(int lower, int upper){
     int range = upper - lower;
     return (int)(Math.random()*range+lower);
