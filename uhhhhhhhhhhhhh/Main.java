d64482b8cd1ff02a567a595b607715e91e24920c
A-Id1ot <gordont13@nycstudents.net>
2025-12-09 14:47:03 +0000

    asd

---
+++ uhhhhhhhhhhhhh/Main.java
@@ -0,0 +1,36 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+  void init(){
+    String m = "Hi yo";
+    System.out.println(e(m));
+  }
+  int e(String s){
+    int randomNum = (int)(Math.random() * 255);
+    System.out.println(randomNum);
+    String newstring = "";
+    int[] newarray = new int[s.length()];
+    System.out.println(s);
+    for(int i = 0; i < s.length(); i++){
+    	int result = s.charAt(i);
+        newarray[i] = result+randomNum;
+      }
+    int[] suffering = new int[newarray.length+1];
+    for(int i = 0; i < newarray.length; i++){
+    	if(newarray[i] % 4 == 0){
+        	suffering[i] = newarray[i]/4;
+      }
+      else{
+     		suffering[i] = newarray[i]*4;
+      }
+    }
+    return randomNum;
+  }
+  int randInt(int lower, int upper){
+    int range = upper - lower;
+    return (int)(Math.random()*range+lower);
+  }
+}
