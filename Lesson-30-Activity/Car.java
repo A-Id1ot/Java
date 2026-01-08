a8c83b83397891c083bb0158c2dc81b3652b92a7
A-Id1ot <gordont13@nycstudents.net>
2026-01-08 14:59:58 +0000

    asd

---
+++ Lesson-30-Activity/Car.java
@@ -0,0 +1,17 @@
+class car{
+    String brand;
+    String color;
+    String model;
+    int year;
+    double value;
+    car(String brand, String color, String model, int year, double value){
+      this.brand=brand;
+      this.color=color;
+      this.model=model;
+      this.year=year;
+      this.value=value;
+    }
+    void honk(){
+      System.out.println("This car is a "+color+" "+year+" "+model+" "+model+" that is worth "+value);
+    }
+}
--- Lesson-30-Activity/Main.java
+++ Lesson-30-Activity/Main.java
@@ -5,22 +5,26 @@ class Main {
   void print(Object o){System.out.println(o);}
   void printt(Object o){System.out.print(o);}
   void init(){
-    class car{
-      String brand;
-      String color;
-      String model;
-      int year;
-      double value;
-      car(String brand, String color, String model, int year, double value){
-        this.brand=brand;
-        this.color=color;
-        this.model=model;
-        this.year=year;
-        this.value=value;
-      }
-      void honk(){
-        System.out.println("honk honk");
-      }
+    car car1=new car("Toyota","grey","Corolla",2010,20000);
+    car car2=new car("Ford","grey","F150",2012,23000);
+    print(car1.brand+" "+car1.model+" "+car1.value);
+    print(car2.brand+" "+car2.model+" "+car2.value);
+  }
+  class car{
+    String brand;
+    String color;
+    String model;
+    int year;
+    double value;
+    car(String brand, String color, String model, int year, double value){
+      this.brand=brand;
+      this.color=color;
+      this.model=model;
+      this.year=year;
+      this.value=value;
+    }
+    void honk(){
+      System.out.println("This car is a "+color+" "+year+" "+brand+" "+model+" that is worth "+value);
     }
   }
   int randInt(int lower, int upper){
