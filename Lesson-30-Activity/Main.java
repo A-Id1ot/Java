286ecd54ae0f6adffda98dab41c18e27124cf226
A-Id1ot <gordont13@nycstudents.net>
2026-01-08 14:48:41 +0000

    asd

--- Lesson-30-Activity/Main.java
+++ Lesson-30-Activity/Main.java
@@ -6,7 +6,21 @@ class Main {
   void printt(Object o){System.out.print(o);}
   void init(){
     class car{
-      
+      String brand;
+      String color;
+      String model;
+      int year;
+      double value;
+      car(String brand, String color, String model, int year, double value){
+        this.brand=brand;
+        this.color=color;
+        this.model=model;
+        this.year=year;
+        this.value=value;
+      }
+      void honk(){
+        System.out.println("honk honk");
+      }
     }
   }
   int randInt(int lower, int upper){
