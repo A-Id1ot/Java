6bb04dc4c9ecb1db1c1d70029ab3675f4400c3c7
A-Id1ot <gordont13@nycstudents.net>
2026-01-20 14:56:29 +0000

    asd

--- Lesson-33-Activity/Main.java
+++ Lesson-33-Activity/Main.java
@@ -6,7 +6,7 @@ class Main {
   void printt(Object o){System.out.print(o);}
   void init(){
     CartItem uhhh=new CartItem("null", 0, 10000, true);
-    System.out.println("The "+uhhh.itemName+" costs "+uhhh.itemPrice+);
+    System.out.println("The "+uhhh.itemName+" costs "+uhhh.itemPrice);
     CartItem cigarettes=new CartItem("cigarettes", 6, 500, false);
     System.out.println("The "+cigarettes.itemName+" costs "+cigarettes.itemPrice);
     CartItem soup=new CartItem("soup",10,12,false);
