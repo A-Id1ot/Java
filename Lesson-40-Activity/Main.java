56a2126f8f6696569b6cf21803a2c3e3beb4cf09
A-Id1ot <gordont13@nycstudents.net>
2026-03-10 13:47:42 +0000

    asd

--- Lesson-40-Activity/Main.java
+++ Lesson-40-Activity/Main.java
@@ -1,32 +1,19 @@
-
 import java.io.IOException;
 import java.sql.*;
 //For compiling on the shell on repl: Same on mac
 //javac -cp sqlite-jdbc-3.23.1.jar: Main.java
-
 //Use for windows
 //javac -cp sqlite-jdbc-3.23.1.jar; ServerExample.java
 class Main {
-
  public static void main(String[] args)throws IOException{
     (new Main()).init();
   }
-
   void print(Object o){ System.out.println(o);}
   void printt(Object o){ System.out.print(o);}
-
   void init() {
-   
-
-    String queryResult="";
-    String sql ="";
-
-   
-
-    
-
-
-    
-     
+      Database db=new Database("jdbc:sqlite:students.db");
+      String sql = "Select * From students where teacher1 = 'BANU';";
+      String queryRequest = db.runSQL(sql, "table-auto");
+		  print(queryRequest);
   }    
 }
