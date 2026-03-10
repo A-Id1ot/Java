3a4bc99136e31edb667b52b3806484e86d1484aa
A-Id1ot <gordont13@nycstudents.net>
2026-03-10 14:01:35 +0000

    asd

--- Lesson-40-Activity/Main.java
+++ Lesson-40-Activity/Main.java
@@ -12,8 +12,20 @@ class Main {
   void printt(Object o){ System.out.print(o);}
   void init() {
       Database db=new Database("jdbc:sqlite:students.db");
-      String sql = "Select * From students where teacher1 = 'BANU';";
+      //String sql = "Select * From cr101 where teacher1 = 'BANU';";
+      //String queryRequest = db.runSQL(sql, "table-auto");
+		  //print(queryRequest);
+      //String sql = "Select * From cr101 where teacher1 = 'PORCHETTA' and period = '10';";
+      //String queryRequest = db.runSQL(sql, "table-auto");
+		  //print(queryRequest);
+      //String sql = "Select distinct studentid From cr101";
+      //String queryRequest = db.runSQL(sql, "table-auto");
+      //print(queryRequest);
+      //String sql = "Select studentid,course,period From cr101 where room in (322,105,106,323) and grade='11';";
+      //String queryRequest = db.runSQL(sql, "table-auto");
+      //print(queryRequest);
+      String sql = "Select * From cr101 where course like 'M%X';";
       String queryRequest = db.runSQL(sql, "table-auto");
-		  print(queryRequest);
-  }    
+      print(queryRequest);
+  }
 }
