5c76f4f1b22436b707bf5525167e4db10f53a0bb
A-Id1ot <gordont13@nycstudents.net>
2026-04-15 14:11:22 +0000

    asd

--- Lesson-49-Classwork/script.js
+++ Lesson-49-Classwork/script.js
@@ -2,7 +2,7 @@
 let employees;
 async function init(){
   
-  let link = "https://Lesson48ClassworkDBServer.ernestoporchett.repl.co";
+  let link = "https://potential-broccoli-v6rvjj9q6qpw26x65-8300.app.github.dev/";
   let route= "/employees"
   info = await fetch(link+route);
   employees = await info.json();
